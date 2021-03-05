b18ee8e904621fc7e96f973791c5a1fdf9b2e9b7
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-04 21:36:42 -0300

    chore: inspection functions

--- teste.php
+++ teste.php
@@ -1,7 +1,5 @@
 <?php
 
-use Alura\Reflection\ClasseExemplo;
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 echo __LINE__ . PHP_EOL;
@@ -13,6 +11,19 @@ $variavel = 'idade';
 echo $$variavel;
 
 $nomeCompletoClasse = 'Alura\Reflection\ClasseExemplo';
+$nomeMetodo = 'metodoPublico';
+
 $objeto = new $nomeCompletoClasse();
 
+if (method_exists($objeto, $nomeMetodo)) {
+  echo PHP_EOL;
+  $objeto->$nomeMetodo();
+  echo PHP_EOL;
+}
+
 var_dump($objeto);
+
+var_dump(get_object_vars($objeto));
+
+var_dump($objeto instanceof \Alura\Reflection\ClasseExemplo);
+var_dump(is_a($objeto, \Alura\Reflection\ClasseExemplo::class));
