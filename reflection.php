96a5ad06ed9c6246ef2dcb989c0ed879a48e89b3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-06 22:00:10 -0300

    chore: inspecting a class

---
+++ reflection.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Alura\Reflection\ClasseExemplo;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$reflectionClass = new ReflectionClass(ClasseExemplo::class);
+
+echo $reflectionClass;
